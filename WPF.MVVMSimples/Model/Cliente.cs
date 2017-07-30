9e5afb4322b1c1079e41abaa3736b2144b89dd60
jeffaraujo <jeff.araujo@gmail.com>
2017-07-29 21:07:58 -0300

    Add project files.

---
+++ WPF.MVVMSimples/Model/Cliente.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using WPF.MVVMSimples.Annotations;
+
+namespace WPF.MVVMSimples.Model
+{
+    public class Cliente : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private int _id;
+
+        public int ID
+        {
+            get { return _id; }
+            set { _id = value; }
+        }
+
+
+        private string _nome;
+
+        public string Nome
+        {
+            get { return _nome; }
+            set { _nome = value; OnPropertyChanged(nameof(Nome)); }
+        }
+
+
+        private string _email;
+
+        public string Email
+        {
+            get { return _email; }
+            set { _email = value; OnPropertyChanged(nameof(Email)); }
+        }
+
+
+
+
+
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ WPF.MVVMSimples/ViewModel/ClienteViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WPF.MVVMSimples.Model;
+
+namespace WPF.MVVMSimples.ViewModel
+{
+    public class ClienteViewModel
+    {
+        public ObservableCollection<Cliente> Clientes { get; set; }
+
+        public void LoadClientes()
+        {
+            var clientes = new ObservableCollection<Cliente>
+            {
+                new Cliente() {Nome = "Jeferson", Email = "email@server1.com", ID = 1},
+                new Cliente() {Nome = "Jefão", Email = "email@server2.com", ID = 1},
+                new Cliente() {Nome = "Mario", Email = "email@server3.com", ID = 1},
+                new Cliente() {Nome = "Pedro", Email = "email@server4.com", ID = 1},
+                new Cliente() {Nome = "Henrique", Email = "email@server5.com", ID = 1},
+                new Cliente() {Nome = "Marina", Email = "email@server6.com", ID = 1}
+            };
+
+            this.Clientes = clientes;
+        }
+    }
+}
